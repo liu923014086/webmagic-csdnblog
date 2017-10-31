44d31ebef15d3cbf41036699e843f798a34ee3d2
liudaxia <923014086@qq.com>
2017-10-31 21:46:00 +0800

    抓取优果图片

--- src/com/liudaxia/cn/news/NewsProcessor.java
+++ src/com/liudaxia/cn/news/NewsProcessor.java
@@ -21,7 +21,7 @@ public class NewsProcessor implements PageProcessor {
 
     private static int size = 0;// 共抓取到的文章数量
 
-    private AtomicInteger processNumber = new AtomicInteger(0);
+    private static AtomicInteger processNumber = new AtomicInteger(0);
 
     // 抓取网站的相关配置，包括：编码、抓取间隔、重试次数等
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
@@ -34,12 +34,24 @@ public class NewsProcessor implements PageProcessor {
     @Override
     // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
     public void process(Page page) {
+        List<String> all1 = page.getHtml().links().regex("http://www\\.ugirls\\.com/Index/Search/.*\\.html").all();
+        page.addTargetRequests(all1);
+
+        List<String> all2 = page.getHtml().xpath("//ul[@class='latest_list']/li/a/@href").all();
+
+        page.addTargetRequests(all2);
+
+        if(page.getUrl().regex("http://www\\.ugirls\\.com/Shop/Detail/.*\\.html").match()) {
+           List<String> all = page.getHtml().xpath("//div[@class='yang auto']/a/img/@src").all();
+           for (String s : all) {
+               processNumber.incrementAndGet();
+               System.out.println("src = [" + s + "]");
+           }
 
 
-         String productName = page.getHtml().xpath("//*[@id=\"J_DetailMeta\"]/div/div/div/div/h1/text()").get();
-         String price = page.getHtml().xpath("//*[@id=\"J_StrPriceModBox\"]/dd/span/text()").get();
-        System.out.println("卫生巾名称 = [" + productName + "]");
-        System.out.println("价格 = [" + price + "]");
+       }
+
+
 
     }
 
@@ -49,11 +61,11 @@ public class NewsProcessor implements PageProcessor {
         System.out.println("【爬虫开始】请耐心等待一大波数据到你碗里来...");
         startTime = System.currentTimeMillis();
         // 从用户博客首页开始抓，开启5个线程，启动爬虫
-        Spider.create(new NewsProcessor()).addUrl("https://detail.tmall.com/item.htm?spm=a230r.1.14.20.67e519261rV04z&id=520169267371&ns=1&abbucket=5")
-            .thread(2).run();
+        Spider.create(new NewsProcessor()).addUrl("http://www.ugirls.com/")
+            .thread(5).run();
         endTime = System.currentTimeMillis();
 
-        WebDriver driver = new ChromeDriver();
+        /*WebDriver driver = new ChromeDriver();
         driver.get("https://detail.tmall.com/item.htm?spm=a230r.1.14.20.67e519261rV04z&id=520169267371&ns=1&abbucket=5");
         try {
             Thread.sleep(1000);
@@ -63,7 +75,7 @@ public class NewsProcessor implements PageProcessor {
 
         String price1 = driver.findElement(By.xpath("//*[@id=\"J_PromoPrice\"]/dd/div/span")).getText();
         System.out.println("args = [" + price1 + "]");
-        driver.close();
-        //System.out.println("【爬虫结束】共抓取" + size + "篇文章，耗时约" + ((endTime - startTime) / 1000) + "秒，已保存到数据库，请查收！");
+        driver.close();*/
+        System.out.println("【爬虫结束】共抓取" + processNumber.get() + "篇文章，耗时约" + ((endTime - startTime) / 1000) + "秒，已保存到数据库，请查收！");
     }
 }
